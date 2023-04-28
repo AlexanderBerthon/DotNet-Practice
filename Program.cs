3d1ee3562b1dd5afa6a2d25e17590451a73bf348
Alex Berthon <Alexjberthon@gmail.com>
2023-04-28 16:56:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string username = Environment.UserName;
+Console.WriteLine(username);
+File.Create("C:\\Users\\" + username + "\\Desktop\\newfile.txt");
